7b17d4208b79735bd8ae239011726e71a1727ded
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-04-26 20:52:53 +0100

    cleanup

--- .github/workflows/logs/validate-logs.sh
+++ .github/workflows/logs/validate-logs.sh
@@ -5,10 +5,10 @@ ls -la $LOG_FILES
 VALIDATION_FILE=failures.txt
 echo -n > "$VALIDATION_FILE" # Empty the file.
 validate() {
-	count=$(grep --perl-regexp --count -z $1 $LOG_FILES || true);
+	count=$(grep --perl-regexp --count --null $1 $LOG_FILES || true);
 	if [ $count -gt 0 ]; then
 		echo "Validation for '$2' failed: found /$1/ $count times." >> "$VALIDATION_FILE";
-		grep --perl-regexp --only-matching -z $1 $LOG_FILES >> "$VALIDATION_FILE";
+		grep --perl-regexp --only-matching --with-filename --line-number -z $1 $LOG_FILES >> "$VALIDATION_FILE";
 		echo "" >> "$VALIDATION_FILE";
 	else
 		echo "Validation for '$2' passed: couldn't find /$1/.";
