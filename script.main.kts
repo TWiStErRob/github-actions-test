e9c61d5a2c8686be8df6d5a9811f86d841ddfa45
Róbert Papp <papp.robert.s@gmail.com>
2024-05-31 01:22:38 +0100

    Update script.main.kts

--- script.main.kts
+++ script.main.kts
@@ -1,33 +1,11 @@
-//@file:RuntimeOptions("-J--illegal-access=deny")
-
-// Based on:
-// main.kts: https://kotlinlang.org/docs/custom-script-deps-tutorial.html
-// HttpClient: https://ktor.io/docs/http-client-engines.html#java
-// Jackson: https://ktor.io/docs/serialization-client.html > Jackson
-// jackson {} config: https://www.baeldung.com/jackson-deserialize-json-unknown-properties
-// Kml lib: https://web.archive.org/web/20170317215912/http://labs.micromata.de/projects/jak/quickstart.html
-// Kml format: exported an example from [My Maps](https://www.google.com/maps/d).
-
-// Implicit dependency: Java 11 (because ktor 2.x is compiled as Class 55)
-// Note: normally these dependencies are listed without a -jvm suffix,
-// but there's no Gradle resolution in play here, so we have to pick a platform manually.
 @file:Repository("https://repo1.maven.org/maven2/")
-@file:DependsOn("org.jetbrains.kotlinx:kotlinx-html-jvm:0.11.0")
 @file:DependsOn("io.ktor:ktor-client-java-jvm:2.3.11")
 @file:DependsOn("io.ktor:ktor-client-content-negotiation-jvm:2.3.11")
 @file:DependsOn("io.ktor:ktor-serialization-jackson-jvm:2.3.11")
-// Override transitively included jaxb-impl:2.2 to avoid warning when marshalling Kml.
-// > Illegal reflective access by com.sun.xml.bind.v2.runtime.reflect.opt.Injector$1 (jaxb-impl-2.2.jar)
-// > to method java.lang.ClassLoader.defineClass(java.lang.String,byte[],int,int)
-@file:DependsOn("com.sun.xml.bind:jaxb-impl:4.0.5")
-@file:DependsOn("de.micromata.jak:JavaAPIforKml:2.2.1")
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import de.micromata.opengis.kml.v_2_2_0.Document
-import de.micromata.opengis.kml.v_2_2_0.Kml
-import de.micromata.opengis.kml.v_2_2_0.Point
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -35,11 +13,6 @@ import io.ktor.client.request.get
 import io.ktor.http.URLBuilder
 import io.ktor.serialization.jackson.jackson
 import kotlinx.coroutines.runBlocking
-import kotlinx.html.a
-import kotlinx.html.body
-import kotlinx.html.html
-import kotlinx.html.p
-import kotlinx.html.stream.createHTML
 import java.io.File
 import java.net.URI
 import java.time.LocalDate
@@ -69,27 +42,6 @@ data class CinemasResponse(
 fun ObjectMapper.cineworldConfig() {
 	configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 }
-
-//<editor-fold desc="JAXB setup for modern Java" defaultstate="collapsed">
-fun initJAXB() {
-	// See https://stackoverflow.com/a/50251510/253468
-	// In jaxb-impl 3.x and 4.x it is necessary to disable this optimization to allow running on Java 17.
-	System.setProperty("com.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize", "true")
-
-	// Disable logging about the optimization being disabled:
-	// > Jul 11, 2023 5:06:10 PM com.sun.xml.bind.v2.runtime.reflect.opt.AccessorInjector <clinit>
-	// > INFO: The optimized code generation is disabled
-
-	// Need to keep a strong reference to the logger instance, otherwise the level customization gets garbage collected.
-	val logger: java.util.logging.Logger = java.util.logging.Logger
-		.getLogger("com.sun.xml.bind.v2.runtime.reflect.opt.AccessorInjector")
-
-	logger.level = java.util.logging.Level.OFF
-	// Load the class to trigger the static initializer that logs the above message.
-	Class.forName("com.sun.xml.bind.v2.runtime.reflect.opt.AccessorInjector")
-	logger.level = null
-}
-//</editor-fold>
 
 fun loadFile(): List<CinemasResponse.CinemasBody.Cinema> {
 	val client = jacksonObjectMapper().apply { cineworldConfig() }.reader()
@@ -125,56 +77,3 @@ fun loadNetwork(now: LocalDate): List<CinemasResponse.CinemasBody.Cinema> {
 
 val now = LocalDate.now()
 val cinemas = loadNetwork(now)
-//println(cinemas.map { it.displayName })
-
-val kml = Kml().apply {
-	feature = Document().apply {
-		name = "Cineworld Cinemas"
-		val niceNow = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).format(now)
-		description = "List of all the cineworld cinemas in the UK on ${niceNow}."
-		createAndAddStyle().apply {
-			id = "cineworld-placemark"
-			createAndSetIconStyle().apply {
-				scale = 1.0
-				createAndSetIcon().apply {
-					href = "https://www.google.com/s2/favicons?domain=www.cineworld.co.uk"
-				}
-			}
-		}
-		createAndAddFolder().apply {
-			name = "Cineworld Cinemas"
-			cinemas.forEach { cinema ->
-				createAndAddPlacemark().apply {
-					name = cinema.displayName
-					description = createHTML(prettyPrint = false).html {
-						body {
-							p {
-								+"Website: "
-								a(href = cinema.link.toString(), target = "_blank") { +cinema.link.toString() }
-							}
-							p {
-								+"Address: "
-								val mapSearch = URLBuilder("https://maps.google.com/maps").apply {
-									parameters.append("q", cinema.address)
-								}.buildString()
-								a(href = mapSearch, target = "_blank") { +cinema.address }
-							}
-						}
-					}.toString()
-					address = cinema.address
-					geometry = Point().addToCoordinates(cinema.longitude, cinema.latitude)
-					styleUrl = "#cineworld-placemark"
-				}
-			}
-		}
-	}
-}
-
-val kmlFile = File("cinemas.kml").apply {
-	delete()
-	initJAXB()
-	kml.marshal(this)
-	writeText(readText().replace("ns2:", "").replace("xmlns:ns2", "xmlns"))
-}
-//print(kmlFile.readText())
-println(kmlFile.absolutePath)
